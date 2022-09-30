deaaab842318a424459d7e7073ace88120544007
Karan Haritwal <karanharitwal@gmail.com>
2022-09-30 19:06:10 +0530

    made changes to all generator js codes so that multiple tables are not getting generated

--- main-website/md5generator.js
+++ main-website/md5generator.js
@@ -2,12 +2,15 @@ const algo = 'md5';
 window.addEventListener('load',()=>{
     const generate= document.querySelector('#generate');
     const text= document.querySelector('#input');
-    const container= document.querySelector('#md5-input');
+    const container= document.querySelector('#table-space');
     generate.addEventListener('click',event =>{
         const hash =  CryptoJS.MD5(text.value);
         const sha1hash= CryptoJS.SHA1(text.value);
+        let row1_data1, row1_data2,row2_data1,row2_data2,row3_data1,row3_data2;
+        if (!document.querySelector('#result_table')){
         const result_table= document.createElement('table');
         result_table.setAttribute('id','result_table');
+        result_table.style.width= "80vw";
         result_table.style.border= "1px solid";
         container.appendChild(result_table);
         let tbody=document.createElement('tbody');
@@ -18,12 +21,18 @@ window.addEventListener('load',()=>{
         tbody.appendChild(row1);
         tbody.appendChild(row2);
         tbody.appendChild(row3);
-        let row1_data1= document.createElement('td');
-        let row1_data2= document.createElement('td');
-        let row2_data1= document.createElement('td');
-        let row2_data2= document.createElement('td');
-        let row3_data1= document.createElement('td');
-        let row3_data2= document.createElement('td');
+        row1_data1= document.createElement('td');
+        row1_data1.setAttribute('id','row1_data1');
+        row1_data2= document.createElement('td');
+        row1_data2.setAttribute('id','row1_data2');
+         row2_data1= document.createElement('td');
+        row2_data1.setAttribute('id','row2_data1');
+         row2_data2= document.createElement('td');
+        row2_data2.setAttribute('id','row2_data2');
+         row3_data1= document.createElement('td');
+        row3_data1.setAttribute('id','row3_data1');
+         row3_data2= document.createElement('td');
+        row3_data2.setAttribute('id','row3_data2');
         tbody.appendChild(row1);
         tbody.appendChild(row2);
         tbody.appendChild(row3);
@@ -33,6 +42,18 @@ window.addEventListener('load',()=>{
         row2.appendChild(row2_data2);
         row3.appendChild(row3_data1);
         row3.appendChild(row3_data2);
+        }
+        else{
+             result_table = document.querySelector('#result_table');
+            row1_data1 = result_table.querySelector('#row1_data1');
+             row1_data2 = result_table.querySelector('#row1_data2');
+             row2_data1 = result_table.querySelector('#row2_data1');
+             row2_data2 = result_table.querySelector('#row2_data2');
+             row3_data1 = result_table.querySelector('#row3_data1');
+             row3_data2 = result_table.querySelector('#row3_data2');
+            }
+
+
         row1_data1.innerHTML="Your String";
         row1_data2.innerHTML=text.value;
         row2_data1.innerHTML="MD5 Hash";
